1b5bb62acc426a18867eb416f227f567bc1ff6d6
Isabella <collantemendezisabella@gmail.com>
2024-04-05 18:33:57 -0500

    Realizamos un servidor para la red del pajarito

---
+++ src/server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const startServer = (options) => {
+  const { port, public_path = "public" } = options;
+
+  const app = express();
+
+  //* para usar la carpeta public o la que se ponga en los .env dinamicamente , se definira como estatica, o sea la que tendra los archivos que se presentaran
+  app.use(express.static(public_path));
+
+  //* response y request siendo * cualquier cosa que escribas en /la lupa
+  //* __dirname el name del directorio mas el archivo al que se redirige, por ultimo siendo la respuesta a enviar
+  app.get("*", (res, req) => {
+    const indexpath = path.join(
+      __dirname + `../../../${public_path}/index.html`
+    );
+    res.sendFile(indexpath);
+  });
+  app.listen(port, () => {
+    console.log(`Escuchando puerto ${port}`)
+  });
+};
+
+module.exports = { startServer };
